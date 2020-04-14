9cc76f838d2329da601589877e704f3e69283480
Shinichiro Funatsu <sfunatsu.git@gmail.com>
2020-04-14 23:53:29 +0900

    refactor3

--- app/src/main/java/com/example/googlemapmock/core/googlemap/GoogleMapMoveActionQuery.kt
+++ app/src/main/java/com/example/googlemapmock/core/googlemap/GoogleMapMoveActionQuery.kt
@@ -2,15 +2,15 @@ package com.example.googlemapmock.core.googlemap
 
 import android.location.Location
 
-fun mapMoveQuery(fromToBlock: (LocationFrom) -> FromTo) {
-    val formTo = fromToBlock(LocationFrom)
+fun mapMoveQuery(fromToBlock: (MapMoveQuery) -> MapMoveQuery.FromTo) {
+    val formTo = fromToBlock(MapMoveQuery)
 }
 
-data class From(val location: Location)
-data class FromTo(val from: Location, val to: Location)
 
-object LocationFrom {
+object MapMoveQuery {
+    data class From(val location: Location)
+    data class FromTo(val from: Location, val to: Location)
     infix fun from(location: Location): From = From(location)
+    infix fun From.to(direction: Location): FromTo = FromTo(this.location, direction)
 }
 
-infix fun From.to(direction: Location): FromTo = FromTo(this.location, direction)
--- app/src/main/java/com/example/googlemapmock/core/sandbox/InterfaceDesign.kt
+++ app/src/main/java/com/example/googlemapmock/core/sandbox/InterfaceDesign.kt
@@ -2,7 +2,7 @@ package com.example.googlemapmock.core.sandbox
 
 import android.location.Location
 import com.example.googlemapmock.core.googlemap.mapMoveQuery
-import com.example.googlemapmock.core.googlemap.to
+import com.example.googlemapmock.core.googlemap.MapMoveQuery.to
 import com.example.googlemapmock.core.location.here
 
 fun moveToMyLocation(direction: Location) {
