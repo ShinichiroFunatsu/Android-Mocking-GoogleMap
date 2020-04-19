8c5b92522749e6a7087f1bead71a39dde54c1c63
Shinichiro Funatsu <sfunatsu.git@gmail.com>
2020-04-19 23:38:14 +0900

    apply singleTapUp

--- app/src/main/java/com/example/googlemapmock/map/BottomNavigationBehavior.kt
+++ app/src/main/java/com/example/googlemapmock/map/BottomNavigationBehavior.kt
@@ -5,22 +5,28 @@ import android.animation.AnimatorListenerAdapter
 import android.animation.TimeInterpolator
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.ViewGroup.MarginLayoutParams
 import android.view.ViewPropertyAnimator
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GestureDetectorCompat
 import com.google.android.material.animation.AnimationUtils
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.properties.Delegates
 
-
+/**
+ * refer HideBottomViewOnScrollBehavior
+ */
 class BottomNavigationBehavior(
     context: Context,
     attributeSet: AttributeSet? = null
-) : CoordinatorLayout.Behavior<BottomNavigationView>(context, attributeSet) {
+) : CoordinatorLayout.Behavior<BottomNavigationView>(context, attributeSet),
+    GestureDetector.OnGestureListener,
+    GestureDetector.OnDoubleTapListener {
 
-    lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onLayoutChild(
         parent: CoordinatorLayout, child: BottomNavigationView, layoutDirection: Int
     ): Boolean {
@@ -31,24 +37,20 @@ class BottomNavigationBehavior(
     }
 
     override fun onTouchEvent(parent: CoordinatorLayout, child: BottomNavigationView, ev: MotionEvent): Boolean {
-        return super.onTouchEvent(parent, child, ev) || handleTouchEvent(ev)
+        return if (gestureDetector.onTouchEvent(ev)) {
+            true
+        } else {
+            super.onTouchEvent(parent, child, ev)
+        }
     }
 
-    private var additionalHiddenOffsetY = 0
     private var currentAnimator: ViewPropertyAnimator? = null
     private var height by Delegates.notNull<Int>()
     private var currentState = STATE_SHOWN
-    private val gestureDetector: GestureDetector by lazy {
-        GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
-            override fun onDown(e: MotionEvent?) = onSingleTap()
-        })
-    }
-
-    private fun handleTouchEvent(
-        ev: MotionEvent
-    ): Boolean {
-        gestureDetector.onTouchEvent(ev)
-        return false
+    private val gestureDetector: GestureDetectorCompat by lazy {
+        GestureDetectorCompat(context, this).apply {
+            setOnDoubleTapListener(this@BottomNavigationBehavior)
+        }
     }
 
     private fun onSingleTap(): Boolean {
@@ -92,7 +94,7 @@ class BottomNavigationBehavior(
         currentState = STATE_HIDDEN
         animateChildTo(
             child,
-            height + additionalHiddenOffsetY,
+            height,
             EXIT_ANIMATION_DURATION.toLong(),
             AnimationUtils.FAST_OUT_LINEAR_IN_INTERPOLATOR
         )
@@ -119,135 +121,61 @@ class BottomNavigationBehavior(
         private const val EXIT_ANIMATION_DURATION = 175
         private const val STATE_SHOWN = 1
         private const val STATE_HIDDEN = 2
+//        private const val DEBUG_TAG = "BottomNavigationBehavior"
     }
-}
 
-//
-//open class HideBottomViewOnScrollBehavior<V : View?> : CoordinatorLayout.Behavior<V> {
-//    private var height = 0
-//    private var currentState = STATE_SCROLLED_UP
-//    private var additionalHiddenOffsetY = 0
-//    private var currentAnimator: ViewPropertyAnimator? = null
-//
-//    constructor(): super() {}
-//    constructor(context: Context?, attrs: AttributeSet?) : super(
-//        context,
-//        attrs
-//    ) {
-//    }
-//
-//    override fun onLayoutChild(
-//        parent: CoordinatorLayout, child: V, layoutDirection: Int
-//    ): Boolean {
-//        val paramsCompat = child!!.layoutParams as MarginLayoutParams
-//        height = child.measuredHeight + paramsCompat.bottomMargin
-//        return super.onLayoutChild(parent, child, layoutDirection)
-//    }
-//
-//    /**
-//     * Sets an additional offset for the y position used to hide the view.
-//     *
-//     * @param child the child view that is hidden by this behavior
-//     * @param offset the additional offset in pixels that should be added when the view slides away
-//     */
-//    fun setAdditionalHiddenOffsetY(
-//        child: V,
-//        @Dimension offset: Int
-//    ) {
-//        additionalHiddenOffsetY = offset
-//        if (currentState == STATE_SCROLLED_DOWN) {
-//            child!!.translationY = height + additionalHiddenOffsetY.toFloat()
-//        }
-//    }
-//
-//    override fun onStartNestedScroll(
-//        coordinatorLayout: CoordinatorLayout,
-//        child: V,
-//        directTargetChild: View,
-//        target: View,
-//        nestedScrollAxes: Int
-//    ): Boolean {
-//        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
-//    }
-//
-//    override fun onNestedScroll(
-//        coordinatorLayout: CoordinatorLayout,
-//        child: V,
-//        target: View,
-//        dxConsumed: Int,
-//        dyConsumed: Int,
-//        dxUnconsumed: Int,
-//        dyUnconsumed: Int
-//    ) {
-//        if (dyConsumed > 0) {
-//            slideDown(child)
-//        } else if (dyConsumed < 0) {
-//            slideUp(child)
-//        }
-//    }
-//
-//    /**
-//     * Perform an animation that will slide the child from it's current position to be totally on the
-//     * screen.
-//     */
-//    fun slideUp(child: V) {
-//        if (currentState == STATE_SCROLLED_UP) {
-//            return
-//        }
-//        if (currentAnimator != null) {
-//            currentAnimator!!.cancel()
-//            child!!.clearAnimation()
-//        }
-//        currentState = STATE_SCROLLED_UP
-//        animateChildTo(
-//            child,
-//            0,
-//            ENTER_ANIMATION_DURATION.toLong(),
-//            AnimationUtils.LINEAR_OUT_SLOW_IN_INTERPOLATOR
-//        )
-//    }
-//
-//    /**
-//     * Perform an animation that will slide the child from it's current position to be totally off the
-//     * screen.
-//     */
-//    fun slideDown(child: V) {
-//        if (currentState == STATE_SCROLLED_DOWN) {
-//            return
-//        }
-//        if (currentAnimator != null) {
-//            currentAnimator!!.cancel()
-//            child!!.clearAnimation()
-//        }
-//        currentState = STATE_SCROLLED_DOWN
-//        animateChildTo(
-//            child,
-//            height + additionalHiddenOffsetY,
-//            EXIT_ANIMATION_DURATION.toLong(),
-//            AnimationUtils.FAST_OUT_LINEAR_IN_INTERPOLATOR
-//        )
-//    }
-//
-//    private fun animateChildTo(
-//        child: V, targetY: Int, duration: Long, interpolator: TimeInterpolator
-//    ) {
-//        currentAnimator = child
-//            .animate()
-//            .translationY(targetY.toFloat())
-//            .setInterpolator(interpolator)
-//            .setDuration(duration)
-//            .setListener(
-//                object : AnimatorListenerAdapter() {
-//                    override fun onAnimationEnd(animation: Animator) {
-//                        currentAnimator = null
-//                    }
-//                })
-//    }
-//
-//    companion object {
-//        protected const val ENTER_ANIMATION_DURATION = 225
-//        protected const val EXIT_ANIMATION_DURATION = 175
-//        private const val STATE_SCROLLED_DOWN = 1
-//        private const val STATE_SCROLLED_UP = 2
-//    }
-//}
+    override fun onDown(event: MotionEvent): Boolean {
+//        Log.d(DEBUG_TAG, "onDown: $event")
+        return true
+    }
+
+    override fun onFling(
+        event1: MotionEvent,
+        event2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+//        Log.d(DEBUG_TAG, "onFling: $event1 $event2")
+        return true
+    }
+
+    override fun onLongPress(event: MotionEvent) {
+//        Log.d(DEBUG_TAG, "onLongPress: $event")
+    }
+
+    override fun onScroll(
+        event1: MotionEvent,
+        event2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+//        Log.d(DEBUG_TAG, "onScroll: $event1 $event2")
+        return true
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+//        Log.d(DEBUG_TAG, "onShowPress: $event")
+    }
+
+    override fun onSingleTapUp(event: MotionEvent): Boolean {
+//        Log.d(DEBUG_TAG, "onSingleTapUp: $event")
+        onSingleTap()
+        return true
+    }
+
+    override fun onDoubleTap(event: MotionEvent): Boolean {
+//        Log.d(DEBUG_TAG, "onDoubleTap: $event")
+        return true
+    }
+
+    override fun onDoubleTapEvent(event: MotionEvent): Boolean {
+//        Log.d(DEBUG_TAG, "onDoubleTapEvent: $event")
+        return true
+    }
+
+    override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
+//        Log.d(DEBUG_TAG, "onSingleTapConfirmed: $event")
+        return true
+    }
+
+}
