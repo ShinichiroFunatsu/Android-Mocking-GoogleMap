9b0532a69bcdd41fea8a032819732a9e210e356f
Shinichiro Funatsu <sfunatsu.git@gmail.com>
2020-04-26 02:51:50 +0900

    simplify

--- app/src/main/java/com/example/googlemapmock/map/MainMapFragment.kt
+++ app/src/main/java/com/example/googlemapmock/map/MainMapFragment.kt
@@ -1,6 +1,5 @@
 package com.example.googlemapmock.map
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -9,21 +8,22 @@ import androidx.lifecycle.observe
 import com.example.googlemapmock.R
 import com.example.googlemapmock.map.behavior.behavior
 import com.example.googlemapmock.map.ktx.GestureDetectorCompat
-import com.example.googlemapmock.map.ktx.LifecycleAwareGestureDetector
 import kotlinx.android.synthetic.main.mainmap_fragment.view.*
 
 class MainMapFragment : Fragment(R.layout.mainmap_fragment) {
 
-    private val singleTapDetector: LifecycleAwareGestureDetector by lazy {
-        GestureDetectorCompat(requireContext().applicationContext)
+    private val singleTapDetector by lazy {
+        return@lazy { view: View ->
+            GestureDetectorCompat(requireContext().applicationContext).apply {
+                view.setOnTouchListener { _, event -> onTouchEvent(event) }
+            }
+        }
     }
     private lateinit var viewModel: MainMapViewModel
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewModel = ViewModelProvider(this).get(MainMapViewModel::class.java)
-        view.setOnTouchListener { _, event -> singleTapDetector.onTouchEvent(event) }
-        singleTapDetector.onSingleTapUp.observe(viewLifecycleOwner) {
+        singleTapDetector(view).onSingleTapUp.observe(viewLifecycleOwner) {
             view.search_omnibox_container.behavior?.onSingleTap()
             view.nav_view.behavior?.onSingleTap()
         }
