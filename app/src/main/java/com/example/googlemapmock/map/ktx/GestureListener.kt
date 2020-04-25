7577f7ae0919f54b71032ae98c43a932c48b948d
Shinichiro Funatsu <sfunatsu.git@gmail.com>
2020-04-25 17:53:42 +0900

    apply delegate-able logging class

--- app/src/main/java/com/example/googlemapmock/map/ktx/GestureListener.kt
+++ app/src/main/java/com/example/googlemapmock/map/ktx/GestureListener.kt
@@ -23,8 +23,11 @@ fun GestureDetectorCompat(
 
 private class SimpleOnGestureListenerImpl(
     private val onSingleTapListener: OnSingleTapListener? = null,
-    private val onLongPressListener: OnLongPressListener? = null
-) : GestureDetector.SimpleOnGestureListener() {
+    private val onLongPressListener: OnLongPressListener? = null,
+    private val listener: GestureDetector.SimpleOnGestureListener =
+        LoggingOnGestureListenerImpl(DEBUG_TAG)
+) : GestureDetector.OnGestureListener by listener,
+    GestureDetector.OnDoubleTapListener by listener {
 
     override fun onDown(event: MotionEvent): Boolean {
         Log.d(DEBUG_TAG, "onDown: $event")
@@ -42,45 +45,69 @@ private class SimpleOnGestureListenerImpl(
         return true
     }
 
-//            override fun onFling(
-//                event1: MotionEvent,
-//                event2: MotionEvent,
-//                velocityX: Float,
-//                velocityY: Float
-//            ): Boolean {
-//                Log.d(DEBUG_TAG, "onFling: $event1 $event2")
-//                return true
-//            }
-//
-//            override fun onScroll(
-//                event1: MotionEvent,
-//                event2: MotionEvent,
-//                distanceX: Float,
-//                distanceY: Float
-//            ): Boolean {
-//                Log.d(DEBUG_TAG, "onScroll: $event1 $event2")
-//                return true
-//            }
-//
-//            override fun onShowPress(event: MotionEvent) {
-//                Log.d(DEBUG_TAG, "onShowPress: $event")
-//            }
-//            override fun onDoubleTap(event: MotionEvent): Boolean {
-//                Log.d(DEBUG_TAG, "onDoubleTap: $event")
-//                return true
-//            }
-//
-//            override fun onDoubleTapEvent(event: MotionEvent): Boolean {
-//                Log.d(DEBUG_TAG, "onDoubleTapEvent: $event")
-//                return true
-//            }
-//
-//            override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
-//                Log.d(DEBUG_TAG, "onSingleTapConfirmed: $event")
-//                return true
-//            }
-
     companion object {
         private const val DEBUG_TAG = "SimpleOnGestureListenerImpl"
+    }
+}
+
+class LoggingOnGestureListenerImpl(tag: String? = null) :
+    GestureDetector.SimpleOnGestureListener() {
+    private val tag = tag ?: DEFAULT_TAG
+
+    override fun onDown(event: MotionEvent): Boolean {
+        Log.d(tag, "onDown: $event")
+        return false
+    }
+
+    override fun onLongPress(event: MotionEvent) {
+        Log.d(tag, "onLongPress: $event")
+    }
+
+    override fun onSingleTapUp(event: MotionEvent): Boolean {
+        Log.d(tag, "onSingleTapUp: $event")
+        return false
+    }
+
+    override fun onFling(
+        event1: MotionEvent,
+        event2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        Log.d(tag, "onFling: $event1 $event2")
+        return false
+    }
+
+    override fun onScroll(
+        event1: MotionEvent,
+        event2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        Log.d(tag, "onScroll: $event1 $event2")
+        return false
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+        Log.d(tag, "onShowPress: $event")
+    }
+
+    override fun onDoubleTap(event: MotionEvent): Boolean {
+        Log.d(tag, "onDoubleTap: $event")
+        return false
+    }
+
+    override fun onDoubleTapEvent(event: MotionEvent): Boolean {
+        Log.d(tag, "onDoubleTapEvent: $event")
+        return false
+    }
+
+    override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
+        Log.d(tag, "onSingleTapConfirmed: $event")
+        return false
+    }
+
+    companion object {
+        const val DEFAULT_TAG = "LoggingOnGestureListenerImpl"
     }
 }
