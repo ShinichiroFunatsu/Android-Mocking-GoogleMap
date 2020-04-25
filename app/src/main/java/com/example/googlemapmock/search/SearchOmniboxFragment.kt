1059324eff6257e952753216d80ad249a05f1392
Shinichiro Funatsu <sfunatsu.git@gmail.com>
2020-04-25 23:30:39 +0900

    add omni search box layout

---
+++ app/src/main/java/com/example/googlemapmock/search/SearchOmniboxFragment.kt
@@ -0,0 +1,20 @@
+package com.example.googlemapmock.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.googlemapmock.R
+
+class SearchOmniboxFragment : Fragment(R.layout.search_omnibox_fragment) {
+
+    companion object {
+        fun newInstance() = SearchOmniboxFragment()
+    }
+
+    private lateinit var viewModel: SearchOmniboxViewModel
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SearchOmniboxViewModel::class.java)
+    }
+}
---
+++ app/src/main/java/com/example/googlemapmock/search/SearchOmniboxViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.googlemapmock.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchOmniboxViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
